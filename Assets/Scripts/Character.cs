b057c15a7a1b9c713b6755a46d043f6cd3023b5e
wdfdd123357@naver.com <wdfdd123357@gmail.com>
2024-03-28 15:52:26 +0900

    Add Character.cs

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,7 @@
+public class Character
+{
+    private int health_Point;           //체력
+    private int attack_Damage;          //공격력
+    private int defensive_Power;        //방어력
+    private int movement_Speed;         //이동속도
+}
